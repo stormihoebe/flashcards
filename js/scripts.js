5d56a48a9feed88eb343f27eb4d5af8f4070a280
Keith Stedman and Stormi Hoebelheinrich <stormi@>
2017-02-14 12:00:18 -0800

    add all other cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 $(function(){
   function slideRight(x){
-    console.log(x);
-    $(x).animate({left:'120%'},300);
+    $(x).animate({left:'120%'},300).addClass('.reset');
   }
+  //Shows definition when clicked
    $(".show-definition").click(function(){
+     //show the sibling definition
      $(this).siblings('.definition').fadeIn(function(){
-       $('.nextcard').slideDown(500);
+       //goes up in the dom to the parent then shows the panel-footer
+       $(this).parent().siblings('.nextcard').slideDown(500);
      });
    });
+   //animates card when done
    $('.nextcard').click(function(){
     slideRight($(this).parent());
-    // console.log($(this).parent());
-
    });
 });
