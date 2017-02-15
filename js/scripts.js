a1a7893002a6696f6602862473d37c0f93d0e911
Keith Stedman and Stormi Hoebelheinrich <stormi@>
2017-02-14 16:08:54 -0800

    add js to properly order cards based on z-index.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 $(function(){
+  var cardArray = [];
+  var cardCount = 20;
+
+  //pushing each card into an array and changing the z-index due to the position stacking in reverse order
+  $('.card').each(function(){
+    $(this).css('z-index',cardCount);
+    cardArray.push(this);
+    cardCount--;
+  });
+
+  //Animation for the card to exit the screen
   function slideRight(x){
     $(x).animate({left:'120%'},300).addClass('.reset');
   }
+
   //Shows definition when clicked
    $(".show-definition").click(function(){
      //show the sibling definition
@@ -10,6 +22,7 @@ $(function(){
        $(this).parent().siblings('.nextcard').slideDown(500);
      });
    });
+
    //animates card when done
    $('.nextcard').click(function(){
     slideRight($(this).parent());
